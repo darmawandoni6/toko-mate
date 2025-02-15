bf1375529353838c94aaa123d28c457c7fd45a21
darmawandoni6 <darmawandoni6@gmail.com>
2025-02-15 14:14:54 +0700

    first comit

--- server/dist/src/utils/set-error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setError = void 0;
-const setError = (code, error) => {
-    const str = { [code]: error.message };
-    return new Error(JSON.stringify(str));
-};
-exports.setError = setError;
